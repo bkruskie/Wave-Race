5cc5f4f15a37be48a4dde5517be1a96c18789693
Brett Kruskie <inviolatequill7@gmail.com>
2026-08-24 00:45:53 -0400

    Update src/game/code_52CD0.c
    Co-authored-by: LLONSIT <76184758+LLONSIT@users.noreply.github.com>

--- src/game/code_52CD0.c
+++ src/game/code_52CD0.c
@@ -528,7 +528,7 @@ void func_8009917C(Vec3f* arg0, Vec3f* arg1, Vec3f* arg2, Vec3f* arg3) {
     arg0->z = (arg1->z + (sp24.vec1.z * COS((s32) ((arg3->y / 360.0f) * 4096.0f)))) -
               (SIN((s32) ((arg3->y / 360.0f) * 4096.0f)) * sp24.vec1.x);
 }
-
+#ifdef NEEDS_RODATA
 void func_8009934C(f32 t, Vec3f* arg1, f32* arg2, f32* arg3, f32* arg4, f32* arg5, f32* arg6);
 
 #ifdef NEEDS_RODATA
